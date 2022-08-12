700eeba4467f372df381073674f86b30ee8666fb
iaii <apoorva@fb.com>
2022-08-12 11:05:07 -0700

    changes

---
+++ CapstoneProject/Models/AXAnimationHelper.h
@@ -0,0 +1,16 @@
+//
+//  AXAnimationHelper.h
+//  CapstoneProject
+//
+//  Created by Apoorva Chilukuri on 8/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AXAnimationHelper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CapstoneProject/Models/AXAnimationHelper.m
@@ -0,0 +1,12 @@
+//
+//  AXAnimationHelper.m
+//  CapstoneProject
+//
+//  Created by Apoorva Chilukuri on 8/12/22.
+//
+
+#import "AXAnimationHelper.h"
+
+@implementation AXAnimationHelper
+
+@end
